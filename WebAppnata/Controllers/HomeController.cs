3abaffc1ed3e2076fa74722f1f54df5ebe7f9b04
nat-hub <nata.shengelia1@gmail.com>
2020-04-04 14:35:39 +0400

    Add project files.

---
+++ WebAppnata/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using WebAppnata.Models;
+
+namespace WebAppnata.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebAppnata/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppnata.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
